bb9f81259e96eea31c8ec95574ca7953a94ea2cb
miiu96 <mihai9625@gmail.com>
2021-05-17 16:35:04 +0300

    extra checks

--- scripts/run-observer.sh
+++ scripts/run-observer.sh
@@ -17,11 +17,21 @@ fi
 PORT=8080
 gnome-terminal -- docker run -p 8080:${PORT} --mount type=bind,source=${OUTPUT_FOLDER}/,destination=/data ${IMAGE_NAME} --validator-key-pem-file="/data/validatorKey.pem" --log-level *:DEBUG --log-save --destination-shard-as-observer=${SHARD}
 
-# check network status
 export ADDRESS_WITH_ROUTE=http://localhost:${PORT}/network/status
 
+#check if 'curl' is installed
+if ! command -v curl &> /dev/null; then
+    apt install -y curl
+fi
+
+#check if 'jq' is installed
+if ! command -v jq &> /dev/null; then
+    apt install -y jq
+fi
+
+
 check_nonce_grows() {
-  FIRST_TIME=0
+  export FIRST_TIME=0
   PREVIOUS_NONCE=0
   RE='^[0-9]+$'
   while true
@@ -36,7 +46,7 @@ check_nonce_grows() {
     if [ "${CURRENT_NONCE}" -gt 0 ]; then
       # check if is first time when current nonce is greater than zero
       if [ "${FIRST_TIME}" -eq 0 ]; then
-        FIRST_TIME=1
+        export FIRST_TIME=1
         PREVIOUS_NONCE=${CURRENT_NONCE}
         sleep 4s
         continue
@@ -55,7 +65,12 @@ export -f check_nonce_grows
 
 TIMEOUT_DURATION=10m
 timeout ${TIMEOUT_DURATION}  bash -c check_nonce_grows
+EXIT_STATUS=$?
 # stop docker container
 CONTAINER_ID=$(docker ps -q  --filter ancestor=${IMAGE_NAME})
 docker stop "${CONTAINER_ID}"
 
+if [ ${EXIT_STATUS} -eq 124 ]; then
+   echo "exit with timeout"
+   exit 1
+fi
